60343e214b3d85535d27d7b6f61b50c7f9c29ddb
mvximumblvck <132149084+mvximumblvck@users.noreply.github.com>
2025-03-27 16:42:38 +0300

    Delete sbis_mane_page.py

--- sbis_mane_page.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-from pages.base_page import BasePage
-
-class SbisMainPage(BasePage):
-    CONTACTS_BUTTON = (By.XPATH, "//a[contains(@href, '/contacts')]")
-
-    def go_to_contacts(self):
-        self.click(self.CONTACTS_BUTTON)
